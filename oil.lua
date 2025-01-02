ea3b12a3245e910a96091c420e6c653c633e6d80
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2025-01-01 21:21:04 -0500

    remove float

--- oil.lua
+++ oil.lua
@@ -11,6 +11,6 @@ return {
 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 	config = function()
 		require("oil").setup()
-		vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
 }
