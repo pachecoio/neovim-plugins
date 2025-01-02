6012279532ff3579103f4723a9cfc576c69a31ad
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2025-01-01 21:03:17 -0500

    fix oil open folder

--- oil.lua
+++ oil.lua
@@ -2,14 +2,16 @@ return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
 	---@type oil.SetupOpts
-	opts = {},
+	opts = {
+		skip_confirm_for_simple_edits = false,
+	},
 	event = "BufEnter",
 	-- Optional dependencies
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 	config = function()
 		require("oil").setup()
-		vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
-		vim.keymap.set("n", "<Leader>m", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "-", "<CMD>Oil --float .<CR>", { desc = "Open current directory" })
+		vim.keymap.set("n", "<Leader>m", "<CMD>Oil --float .<CR>", { desc = "Open current directory" })
 	end,
 }
