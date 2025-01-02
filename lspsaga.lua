5ba923b1a197ea3bf6fc600f5524f156748ce54b
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2025-01-01 21:32:49 -0500

    readd lspsaga

--- lspsaga.lua
+++ lspsaga.lua
@@ -13,17 +13,31 @@
 -- map("n", "]d", ":Lspsaga diagnostic_jump_next<cr>", { desc = "Next diagnostic" })
 
 return {
-	-- {
-	-- 	"nvimdev/lspsaga.nvim",
-	-- 	lazy = false,
-	-- 	config = function()
-	-- 		require("lspsaga").setup({})
-	-- 	end,
-	-- 	dependencies = {
-	-- 		"nvim-treesitter/nvim-treesitter",
-	-- 		"nvim-tree/nvim-web-devicons",
-	-- 	},
-	-- },
+	{
+		"nvimdev/lspsaga.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("lspsaga").setup({})
+			-- Toggle code actions
+			local map = vim.keymap.set
+			map("n", "<leader>rn", ":Lspsaga rename<cr>", { desc = "Lspsaga Rename" })
+			map("n", "<leader>ca", ":Lspsaga code_action<cr>", { desc = "Lspsaga Code action" })
+			map("n", "<leader>cd", ":Lspsaga peek_definition<cr>", { desc = "Peek definition" })
+			map("n", "<S-k>", ":Lspsaga hover_doc<cr>", { desc = "Hover doc" })
+
+			map("n", "gd", ":Lspsaga goto_definition<cr>", { desc = "Go to definition" })
+			map("n", "<leader>co", ":Lspsaga outline<cr>", { desc = "Outline" })
+
+			-- Show diagnostics in line with lspsaga
+			map("n", "gl", ":Lspsaga show_line_diagnostics<cr>", { desc = "Show line diagnostics" })
+			map("n", "[d", ":Lspsaga diagnostic_jump_prev<cr>", { desc = "Prev diagnostic" })
+			map("n", "]d", ":Lspsaga diagnostic_jump_next<cr>", { desc = "Next diagnostic" })
+		end,
+	},
 	-- Replace lspsaga with nvim-lsputils
 	{
 		"RishabhRD/nvim-lsputils",
