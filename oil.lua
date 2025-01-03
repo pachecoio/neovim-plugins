16943bd90feffa45f6e096448bcf18b18fe50212
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2025-01-03 07:50:43 -0500

    Extend oil config

--- oil.lua
+++ oil.lua
@@ -11,6 +11,7 @@ return {
 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 	config = function()
 		require("oil").setup()
-		vim.keymap.set("n", "-", "<CMD>Oil .<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "_", "<CMD>Oil .<CR>", { desc = "Open root directory" })
 	end,
 }
