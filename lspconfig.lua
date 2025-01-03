9742b80662c8fe4284411d0d7d5fda6a3d6e0f78
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2025-01-02 19:15:02 -0500

    include custom lspconfig

---
+++ lspconfig.lua
@@ -0,0 +1,42 @@
+local lspconfig = require "lspconfig"
+
+-- EXAMPLE
+local servers = {
+  "html",
+  "cssls",
+  "pyright",
+  "gopls"
+}
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
+configuring single server, example: typescript
+lspconfig.ts_ls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+}
+
+lspconfig.ts_ls.setup {
+  on_attach = nvlsp.on_attach,
+  root_dir = lspconfig.util.root_pattern "package.json",
+  single_file_support = false,
+  capabilities = nvlsp.capabilities,
+}
+
+lspconfig.elixirls.setup {
+  on_attach = nvlsp.on_attach,
+  capabilities = nvlsp.capabilities,
+  -- requires installing elixir-ls with homebrew
+  cmd = { "/opt/homebrew/Cellar/elixir-ls/0.26.1/bin/elixir-ls" },
+}
+
+return {}
