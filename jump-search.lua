9d01a3cf106fa94a2dd08b5e6f863a47366ac568
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2025-01-07 07:10:40 -0500

    add specific mappings for leap to avoid conflicts

--- jump-search.lua
+++ jump-search.lua
@@ -1,19 +1,23 @@
 return {
-	"ggandor/leap.nvim",
-	lazy = false,
-	dependencies = {
-		{ "tpope/vim-repeat" },
-	},
-	config = function()
-		local leap = require("leap")
-		leap.create_default_mappings()
+  "ggandor/leap.nvim",
+  lazy = false,
+  dependencies = {
+    { "tpope/vim-repeat" },
+  },
+  config = function()
+    local leap = require("leap")
+    -- leap.create_default_mappings()
 
-		-- Define equivalence classes for brackets and quotes, in addition to
-		-- the default whitespace group.
-		leap.opts.equivalence_classes = { " \t\r\n", "([{", ")]}", "'\"`" }
+    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
+    vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+    -- vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
 
-		-- Use the traversal keys to repeat the previous motion without explicitly
-		-- invoking Leap.
-		require("leap.user").set_repeat_keys("<enter>", "<backspace>")
-	end,
+    -- Define equivalence classes for brackets and quotes, in addition to
+    -- the default whitespace group.
+    leap.opts.equivalence_classes = { " \t\r\n", "([{", ")]}", "'\"`" }
+
+    -- Use the traversal keys to repeat the previous motion without explicitly
+    -- invoking Leap.
+    require("leap.user").set_repeat_keys("<enter>", "<backspace>")
+  end,
 }
